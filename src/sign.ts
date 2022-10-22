ccab30a4ff5bf8a52499707afa5b7b3270fe4140
choeqq <k.chola@op.pl>
2022-10-22 19:05:41 +0200

    sign

---
+++ src/sign.ts
@@ -0,0 +1,54 @@
+import crypto from 'crypto';
+
+export interface Options {
+  expiresIn?: number;
+}
+
+export interface SingInput {
+  payload: object;
+  secret: string;
+  options?: Options;
+}
+
+const defaultOptions = {
+  expiresIn: 8.64e7,
+};
+
+interface CreateSignatureInput {
+  secret: string;
+  encodedHeader: string;
+  encodedPayload: string;
+}
+
+export function createSignature({
+  secret,
+  encodedHeader,
+  encodedPayload,
+}: CreateSignatureInput) {
+  return crypto
+    .createHmac('sha256', secret)
+    .update(encodedHeader + '.' + encodedPayload)
+    .digest('base64');
+}
+
+function sign({ payload, secret, options = {} }: SingInput) {
+  const mergedOptions = { ...defaultOptions, ...options };
+
+  const header = { alg: 'HS256', type: 'JWT' };
+
+  const encodedHeader = Buffer.from(JSON.stringify(header)).toString('base64');
+
+  const date = Date.now();
+
+  const expiresIn = date + mergedOptions.expiresIn;
+
+  const encodedPayload = Buffer.from(
+    JSON.stringify({ ...payload, exp: expiresIn })
+  ).toString('base64');
+
+  const signature = createSignature({ encodedPayload, encodedHeader, secret });
+
+  return `${encodedHeader}.${encodedPayload}.${signature}`;
+}
+
+export default sign;
